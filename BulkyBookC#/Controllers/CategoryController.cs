bcbe7370755638011a1d91a270a114cfa1a48e17
wildcard <claudio__lucas@hotmail.com>
2024-11-26 01:08:38 +0000

    Finished Video 37

---
+++ BulkyBookC#/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookC_.Data;
+using BulkyBookC_.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookC_.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+    }
+}
--- BulkyBookC#/Data/ApplicationDbContext.cs
+++ BulkyBookC#/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookC_.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookC_.Data
 {
@@ -6,7 +7,18 @@ namespace BulkyBookC_.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+          
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Drama", DisplayOrder = 1 },
+                new Category { Id = 3, Name = "Horror", DisplayOrder = 1 },
+                new Category { Id = 4, Name = "Fiction", DisplayOrder = 1 }
+                );
         }
     }
 }
