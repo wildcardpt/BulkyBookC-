68035c49cb5167c481debcb0b1e827aff42736e1
wildcard <claudio__lucas@hotmail.com>
2024-11-26 20:38:10 +0000

    Video 39 Implemented Tag helpers

--- BulkyBookC#/Models/Category.cs
+++ BulkyBookC#/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookC_.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyBookC_.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
